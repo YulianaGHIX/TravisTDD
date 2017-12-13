5af76601b18ffed103a1adb7129c6289bed1bf88
Yuliana Garcia Hidalgo <ygarcia@adquem.com>
2017-12-13 16:56:53 -0600

    maven proyect

---
+++ src/test/java/travismaven/TravisTest.java
@@ -0,0 +1,28 @@
+package travismaven;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TravisTest {
+	@Test
+	public void testAdd() {
+		Travis calc = new Travis();
+		assertEquals(calc.add(1, 1), 2);
+	}
+	@Test
+	public void testSub() {
+		Travis calc = new Travis();
+		assertEquals(calc.sub(1, 1), 0);
+	}
+	@Test
+	public void testMul() {
+		Travis calc = new Travis();
+		assertEquals(calc.mul(3, 5), 15);
+	}
+	@Test
+	public void testDiv() {
+		Travis calc = new Travis();
+		assertEquals(calc.div(8, 2), 4);
+	}
+}
